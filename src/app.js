403fc30fc0d6fe0dfe070af9260aff7c594d1ad1
dongcheolpark <head02325@gmail.com>
2021-05-28 16:40:08 +0900

    a

--- src/app.js
+++ src/app.js
@@ -1,39 +1,21 @@
-const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { prefix, token} = require("./config.json");
-
-client.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for(const files of commandFiles) {
-  const command = require(`./commands/${files}`);
-  client.commands.set(command.name,command);
-}
+const token = require("./token.json");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
-client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-
-  if (!client.commands.has(command)) {
-    message.channel.send('잘못된 명령입니다.');
-    return;
-  } 
-
-	try {
-		client.commands.get(command).execute(message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+  if (msg.content === '느금마') {
+    msg.reply('정해윤');
+  }
+  if (msg.content === '니애미') {
+    msg.reply(':1620207266916:');
+  }
 });
 
-client.login(token);
+client.login(token.token);
