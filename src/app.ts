c78abf882a0b945c1fdd4ead2107d9dc8e3c1f14
dongcheolpark <head02325@soongsil.ac.kr>
2021-05-29 14:28:31 +0900

    a

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,13 @@ import { Client } from 'discord.js';
 import { prefix, token } from './config.json';
 import httpserach from './functions/httpserach';
 import { AxiosError } from 'axios';
+import {TeamList} from './classes/List';
+import Teams from './functions/Team';
+
 
 const client = new Client();
+
+let TeamData = new TeamList();
 
 client.on('ready', () => {
 });
@@ -16,26 +21,39 @@ client.on('message', message => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args[0];
 
-	if(command === '응애') {
-		message.channel.send('애응');
-	}
-	else if(command === '느금마') {
-		message.channel.send('니애미');
-	}
-	else if(command === '전적') {
-		httpserach.makeEmbedMessage(args[1]).then((embmsg) => {
-			message.channel.send(embmsg);
-		})
-		.catch((error) => {
-			if ((error as AxiosError).isAxiosError) {
-				const axioserror : AxiosError = error;
-				console.log(axioserror.request);
+	try {
+		if(command === '응애') {
+			message.channel.send('애응');
+		}
+		else if(command === '느금마') {
+			message.channel.send('니애미');
+		}
+		else if(command === '전적') {
+			httpserach.makeEmbedMessage(args[1]).then((embmsg) => {
+				message.channel.send(embmsg);
+			})
+			.catch((error) => {
+				if ((error as AxiosError).isAxiosError) {
+					const axioserror : AxiosError = error;
+					console.log(axioserror.request);
+				}
+				console.log(error.message);
+				message.channel.send('에러가 발생했습니다.');
+			})
+		}
+		else if(command === '팀') {
+			if(args[1] === '추가') {
+				if(args[2] === null) {
+					throw '';
+				}
+				message.channel.send(Teams.AddTeam(TeamData,args[2]));
 			}
-			console.log(error.message);
-			message.channel.send('에러가 발생했습니다.');
-		})
+		}
+		else {
+			throw '';
+		}
 	}
-	else {
+	catch {
 		message.channel.send("잘못된 명령입니다.");
 	}
 });
