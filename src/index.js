c26aaa5fe0164878d8a5eef07836eade635863a4
dongcheolpark <head02325@soongsil.ac.kr>
2021-06-13 19:29:06 +0900

    a

--- src/index.js
+++ src/index.js
@@ -19,10 +19,30 @@ client.on('ready', () => {
 	console.log("봇이 준비되었습니다.")
 });
 
+const nameofkim = ['애미없는년','애비없는년','엄마없는년','아빠없는년'];
+let cnt = 0;
+let chk = 0;
+
+client.on('guildMemberUpdate', (member,member2)=> {
+	if(member.id == '586928806556598274') {
+		if(chk == 1) {
+			chk = 0;
+			return;
+		}
+		member.setNickname(nameofkim[cnt]);
+		if(cnt == nameofkim.length-1) {
+			cnt = 0;
+		}
+		else {
+			cnt++;
+		}
+		console.log("닉네임을 성공적으로 바꾸었습니다." + member2.nickname);
+		chk = 1;
+	}
+})
+
 
 client.on('message', msg => {
-	const kim = msg.guild.members.cache.find(a => a.id == '586928806556598274')
-	kim.setNickname('애미없는년')
 	if (!msg.content.startsWith(prefix) || msg.author.bot) return
 
 	const args = msg.content.slice(prefix.length).trim().split(/ +/);
