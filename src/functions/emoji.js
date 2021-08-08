bd0a424ff7e799d8cae4b7f397d89ac0050037ed
dongcheolpark <head02325@gmail.com>
2021-08-08 21:10:42 +0900

    a

--- src/functions/emoji.js
+++ src/functions/emoji.js
@@ -2,16 +2,21 @@ const discord = require('discord.js');
 
 module.exports = {
 	change_emoji(client,msg) {
-		const args = msg.content.trim().split(/ +/g);
-		let emoji = args[0];
-		emoji = emoji.substring(3, emoji.length - 1);
-		const emojiarray = emoji.split(':');
-		const botemoji = client.emojis.cache.get(emojiarray[1]);
-        let embmsg = new discord.MessageEmbed()
-			.setTitle(msg.member.nickname)
-			.setImage(botemoji.url)
-			.setColor(msg.member.displayColor)
-		msg.delete();
-		msg.channel.send(embmsg);
+		try {
+			const args = msg.content.trim().split(/ +/g);
+			let emoji = args[0];
+			emoji = emoji.substring(3, emoji.length - 1);
+			const emojiarray = emoji.split(':');
+			const botemoji = client.emojis.cache.get(emojiarray[1]);
+			let embmsg = new discord.MessageEmbed()
+				.setTitle(msg.member.nickname)
+				.setImage(botemoji.url)
+				.setColor(msg.member.displayColor)
+			msg.delete();
+			msg.channel.send(embmsg);
+		}
+		catch {
+			msg.channel.send('잘못된 이모티콘입니다.');
+		}
 	}
 }
