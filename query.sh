0a4c7de8680cdbd4783128fef7aee807600b0c40
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2021-08-16 20:57:34 +0900

    a

---
+++ query.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+API_KEY=AIzaSyBzJxtbyLQAVWNwey0_sq2eyxjc3gsdXrI
+ENGINE_ID=c2a9929cdd7c7277a
+curl "https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${ENGINE_ID}&q=javalove93"
+
